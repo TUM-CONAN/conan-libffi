06d616ad7645cc86aa5cfd7c4629852d91d614f5
Ulrich Eck <ulrich.eck@tum.de>
2020-04-25 12:16:46 +0200

    adapted to camp conan environment

--- conanfile.py
+++ conanfile.py
@@ -3,14 +3,15 @@ from conans import AutoToolsBuildEnvironment, ConanFile, tools
 
 class LibffiConan(ConanFile):
     name = "libffi"
-    version = tools.get_env("GIT_TAG", "3.3-rc0")
+    version = tools.get_env("GIT_TAG", "3.3")
     settings = "os", "compiler", "build_type", "arch"
     license = "MIT"
     description = "A portable, high level programming interface to various calling conventions"
+    generators = "pkgconf"
 
     def build_requirements(self):
-        self.build_requires("generators/1.0.0@%s/stable" % self.user)
-        self.build_requires("autotools/[>=1.0.0]@%s/stable" % self.user)
+        self.build_requires("generators/1.0.0@camposs/stable")
+        self.build_requires("pkgconf/1.6.3@camposs/stable")
 
     def source(self):
         tools.get("https://github.com/libffi/libffi/archive/v%s.tar.gz" % self.version)
